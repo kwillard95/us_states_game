0fed2676c6cadca9276e84ffd6ff18eb91aecb78
Kelly Willard <kwillard95@gmail.com>
2024-08-26 21:52:16 -0700

    write states to study on exit

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import turtle
 import pandas
 import time
 
-
-
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
@@ -35,14 +33,18 @@ def display_remaining_states():
         pen.write(remaining_states[index], False, "left", ("Arial", 18, "normal"))
 
 
-def display_state_name(rowData):
-    state_name = rowData.state.values[0]
-    x_cor = rowData.x.values[0]
-    y_cor = rowData.y.values[0]
+def write_remaining_states():
+    remaining_states_dataframe = pandas.DataFrame({"States to Study": remaining_states})
+    remaining_states_dataframe.to_csv("states_to_study.csv")
+
+
+def display_state_name(row_data):
+    state_name = row_data.state.item()
+    x_cor = row_data.x.item()
+    y_cor = row_data.y.item()
     pen.setpos(x_cor, y_cor)
     pen.write(state_name, False, "center")
     remaining_states.remove(state_name)
-    # states_guessed.append(state_name)
 
 
 def get_state_data(guess):
@@ -56,7 +58,6 @@ def start_game():
     while game_running:
         user_guess = screen.textinput(title=f"{50 - len(remaining_states)}/50 States Correct",
                                         prompt=f"Type the name of a state.")
-        print(user_guess)
         if user_guess is not None:
             state_guess_data = get_state_data(user_guess.title())
             if state_guess_data is not None:
@@ -68,16 +69,11 @@ def start_game():
         elif user_guess is None:
             game_running = False
             display_remaining_states()
+            write_remaining_states()
 
         screen.update()
 
 
 start_game()
 
-
-def get_mouse_coor(x, y):
-    print(x, y)
-
-
-screen.onscreenclick(get_mouse_coor)
 turtle.mainloop()
